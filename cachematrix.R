b541ea7f13a2bef25c65e13e662058592ab06eb7
Squalimander <101134416+Squalimander@users.noreply.github.com>
2022-07-13 16:44:47 -0700

    First and only commit for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set of functions that creates a class for a matrix that can cache its inverse
+##
 
-## Write a short comment describing this function
+## Constructor function to create a matrix object capeable of caching its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_matrix<-NULL
+        set<-function(y){
+                x<<-y
+                inv_matrix<<-NULL
+        }
+        get<- function() x
+        setInverse <- function(Temp_Inv_Matrix) inv_matrix <<-Temp_Inv_Matrix
+        getInverse <- function() inv_matrix
+        list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## Function to calculate the inverse and store it in the inv_matrix value in the
+## makeCacheMatrix class
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv<- x$getInverse()
+        if(!is.null(inv)){
+                message("data already cached")
+                return(inv)
+        }
+        matrix<-x$get()
+        inv_matrix<-solve(matrix)
+        x$setInverse(inv_matrix)
+        inv_matrix
 }
